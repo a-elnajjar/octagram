2b5f6c7554df40b797b59c4bcaed8a6d30d77169
abdalla.el.najjar <a-elnajjar@hotmail.com>
2025-04-05 08:20:22 -0400

    Initial Commit

---
+++ octagram/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  octagram
+//
+//  Created by Abdalla Elnajjar on 2025-04-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ octagram/octagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  octagramApp.swift
+//  octagram
+//
+//  Created by Abdalla Elnajjar on 2025-04-05.
+//
+
+import SwiftUI
+
+@main
+struct octagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
